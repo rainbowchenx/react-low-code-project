fce718c3434f46e8680116447056005120fd7761
rainbowchen <1664373735@qq.com>
2023-08-16 22:28:51 +0800

    feat:支持换行功能

--- src/componnets/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/componnets/QuestionComponents/QuestionParagraph/Component.tsx
@@ -4,10 +4,19 @@ import { Typography } from 'antd'
 const { Paragraph } = Typography
 const Component: FC<QuestionParagraphPropType> = (props: QuestionParagraphPropType) => {
   const { text = '', isCenter = false } = { ...QuestionParagraphDefaultProps, ...props }
+  // const t = text.replaceAll('\n', '<br/>')
+  const textList = text.split('\n')
 
   return (
     <Paragraph style={{ textAlign: isCenter ? 'center' : 'start', marginBottom: '0' }}>
-      {text}
+      {/* <span dangerouslySetInnerHTML={{ __html: t }}></span> */}
+      {/* 不要用上述的方法，可以在其中插入任意内容 */}
+      {textList.map((item, index) => (
+        <span key={index}>
+          {index > 0 && <br />}
+          {item}
+        </span>
+      ))}
     </Paragraph>
   )
 }
